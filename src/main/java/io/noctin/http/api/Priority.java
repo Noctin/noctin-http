2d94881fddef513e979f0456ddde0dfbbb7c12c3
Marethyun <bacci.ange@gmail.com>
2018-11-13 15:03:48 +0100

    Added few API objects

---
+++ src/main/java/io/noctin/http/api/Priority.java
@@ -0,0 +1,12 @@
+package io.noctin.http.api;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Priority {
+    long value() default 0L;
+}
---
+++ src/main/java/io/noctin/http/api/Stage.java
@@ -0,0 +1,4 @@
+package io.noctin.http.api;
+
+public class Stage {
+}
